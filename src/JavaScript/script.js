b2841ad535717f03b25eb64debcc2c7c4059f2e1
Alec-T0 <tayloralec816@gmail.com>
2023-10-16 14:07:51 +0100

    Animated Background
    Animated transition for the background image. Problem with looping back from the end to the start, goes from opacity 1-0-1 causing a white screen

--- src/JavaScript/script.js
+++ src/JavaScript/script.js
@@ -1,16 +1,10 @@
-let curImg = 1;
-let bgImgs = document.querySelectorAll(".bg-imgs img");
-    setInterval(() => {
-        /* if first image (curImg = 0), then unload last image */
-        let prevImg = curImg? curImg-1: bgImgs.length-1;
-
-        bgImgs[prevImg].style.opacity = 0;
-        bgImgs[curImg].style.opacity = 100;
-        curImg++;
-
-        /* if last image, go back to first image */
-        if (curImg >= bgImgs.length) {
-            curImg = 0;
-        }
-    / *every 10 seconds */
-}, 50000);
+var images = document.querySelectorAll('#container img');
+var currentImage = 0;
+setInterval(function() {
+    images[currentImage].style.opacity = 0;
+    currentImage++;
+    if (currentImage >= images.length) {
+    currentImage = 1;
+    }
+    images[currentImage].style.opacity = 1;
+}, 1000);
